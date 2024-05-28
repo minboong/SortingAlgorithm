416e3ff919f126068de2deecd1a27e277cfa4bc7
minboong <mj7392@naver.com>
2024-05-28 16:29:33 +0900

    deleted:    heapsort.py 	modified:   sort.py

--- sort.py
+++ sort.py
@@ -92,6 +92,38 @@ def quickSort(list):
     return quickSort(leftList)+pivot+quickSort(rightList)
 
 
+def percolateDown(arr,targetIndex):
+    leftChild=2*targetIndex+1
+    rightChild=2*targetIndex+2
+    if leftChild>len(arr)-1:
+        return arr    
+    if arr[targetIndex] < arr[leftChild]:
+        arr=exchange(arr,leftChild,targetIndex)
+        arr=percolateDown(arr,leftChild)
+        arr=percolateDown(arr,rightChild)
+    if rightChild>len(arr)-1:
+        return arr
+    if arr[targetIndex] < arr[rightChild]:
+        arr=exchange(arr,rightChild,targetIndex)
+        arr=percolateDown(arr,leftChild)
+        arr=percolateDown(arr,rightChild)
+    return arr
+
+
+def buildHeap(arr):
+    targetIndex=(len(arr)-2)//2  #len(arr)-1의 부모노드부터 시작
+    while targetIndex >= 0:
+        arr=percolateDown(arr,targetIndex)
+        targetIndex-=1
+
+def heapSort(arr):
+    result=[]
+    while len(arr)>0:
+        buildHeap(arr)
+        result.append(arr[0])
+        arr=arr[1:]
+    return result
+
         
 
     
@@ -100,6 +132,8 @@ list=[]
 for i in range(10):
     list.append(random.randint(0,100))
 #print(list)
-list=quickSort(list)
+list=heapSort(list)
+for i in list:
+    print(i)
 #print(list)
 
